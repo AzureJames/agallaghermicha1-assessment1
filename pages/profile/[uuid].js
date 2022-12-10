5d5266608e05b551cedb64c07fb12fd9c1d64e16
Azure James G.M <77216235+AzureJames@users.noreply.github.com>
2022-12-09 20:39:58 -0700

    idk2

--- pages/profile/[uuid].js
+++ pages/profile/[uuid].js
@@ -1,105 +1,33 @@
-export default function Uuid( {
-  favoriteColor,
-    createdAt,
-    name,
-    avatar,
-    id
-} ) { //{uuid}  
+// import { json } from "stream/consumers";
 
-  console.log(name);
-    const container = {
-        maxWidth: '1000px',
-    margin: '0 auto'    }
+function SingleDeveloperPage({ avatar, fullName, city, ...bio}) {
+  return (
+    <div className="max-w-2xl mx-auto py-20">
+      <p>{fullName} </p>
+      <p>{city}</p> 
+    </div>
+  );
+}
 
-    return ( 
-        <div style={container}>
-            <h1>Developer Profile</h1> 
-            <p>{favoriteColor}</p> 
-            <p>{id}</p>
-            <p>{createdAt}</p>
-            <p>{name} {avatar}</p>
-        </div>
-    )}
+export default SingleDeveloperPage;
 
+export async function getStaticPaths() {
+  let res = await fetch('https://63938671ab513e12c510ef40.mockapi.io/devs') 
+  let devs = await res.json();
+  const paths = devs.map((dev) => ({ params: { uuid: dev.id } }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
+export async function getStaticProps({ params }) {
+  let res = await fetch('https://63938671ab513e12c510ef40.mockapi.io/devs') 
+  let devs = await res.json();
+  const dev = devs.find((dev) => dev.id === params.uuid);
+  const bio = { ...dev};
 
-
-
-
-
-    export async function getStaticPaths() {
-      let users = await fetch('https://63938671ab513e12c510ef40.mockapi.io/devs');
-      users = await users.json();
-      const paths = users.map((user) => ({ params: { uuid: user.id } }));
-      return {
-        paths,
-        fallback: false,
-      };
-    }
-    
-
-
-
-    // export async function getStaticPaths() {
-    //   return {
-    //     paths: [{ params: { uuid: '1' } }, 
-    //     { params: { uuid: '2' } },
-    //     { params: { uuid: '3' } },
-    //     { params: { uuid: '4' } },
-    //     { params: { uuid: '5' } },
-    //     { params: { uuid: '6' } },
-    //     { params: { uuid: '0' } },
-    //     ],
-    //     fallback: false, // can also be true or 'blocking'
-    //   }
-    // }
-
-
-
-
-
-
-
-    export async function getStaticProps({params}) {
-        // Call an external API endpoint to get posts.
-        // You can use any data fetching library
-        let res;
-        res = await fetch('https://63938671ab513e12c510ef40.mockapi.io/devs') 
-        let users = await res.json()
-        const user = users.find((user) => user.id-1 == params.id);
-        const bio = { ...user};
-        //let user = data.find(item=> item.id-1 == params.uuid)
-        return {
-          props: {
-            bio,
-          },
-        }
-      }
-
-     
-      
-
-    //   export async function getStaticPaths(context) {  
-    //     // Call an external API endpoint to get posts.
-    //     // You can use any data fetching library
-        
-    //     let res;
-    //     let data;
-    //     res = await fetch('https://63938671ab513e12c510ef40.mockapi.io/devs') 
-    //     data = await res.json()
-        
-    
-    //     // return {
-    //     //   props: {
-    //     //     devs,
-    //     //   },
-    //     const paths = data.map(items => ({params:{uuid:items.id}}))
-
-    //     return {
-    //       paths,
-    //       fallback: true
-    //     }
-    // }
-      
-      
-    
+  return {
+    props: bio,
+  };
+}
